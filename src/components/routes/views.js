4a85b384d2bff4362fddedccecfc846caabe9fea
Luis <Luis@LAPTOP-0EFHT9GS>
2021-10-10 13:18:10 -0500

    Refactor routes, home is list page

--- src/components/routes/views.js
+++ src/components/routes/views.js
@@ -3,7 +3,7 @@ import EventsListPage from "../views/EventsListPage";
 import ProfileView from "../views/ProfileView";
 import Register from "../views/Register";
 import TestView from "../views/TestView";
-import Home from "../views/Home/index";
+//import Home from "../views/Home/index";
 
 
 
@@ -13,10 +13,6 @@ const a = (props) => {
     <h1>Esto es a</h1>
   )}
   
-const b = () => (
-<h1>Esto es b</h1>
-)
-
 const blog = () => (
 <h1>Esto es un blog con muchas entradas</h1>
 )
@@ -24,7 +20,7 @@ const blog = () => (
 const isPublicFalse = true
 
 export const views = {
-    '/': {view: Home, name: 'Home', isPublic: true},
+    '/': {view: ElderlyListPage, name: 'Home', isPublic: true},
     '/profileview': {view: ProfileView, name: 'ProfileView', isPublic: isPublicFalse},
     '/ancianato': {view: ProfileView, name: 'ProfileView', isPublic: true},
     '/a': {view: a, name: 'A', isPublic: true},    
