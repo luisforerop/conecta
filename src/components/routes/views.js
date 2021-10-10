900d1d6c0c21ef57418a256828c15914b6ef079d
MariaAlejandraBoadaRodriguez <alejandrarodriguez2508@gmail.com>
2021-10-10 07:09:29 -0500

    Arreglo de conflictos

--- src/components/routes/views.js
+++ src/components/routes/views.js
@@ -19,14 +19,6 @@ const blog = () => (
 <h1>Esto es un blog con muchas entradas</h1>
 )
 
-<<<<<<< HEAD
-const lista = () => (
-  <h1>Esto es una lista</h1>
-)
-  
-
-=======
->>>>>>> 90d3de76ab9c9bb87f582c5c331998459ebb2d6d
 const isPublicFalse = true
 
 export const views = {
@@ -34,14 +26,7 @@ export const views = {
     '/profileview': {view: ProfileView, name: 'ProfileView', isPublic: isPublicFalse},
     '/ancianato': {view: ProfileView, name: 'ProfileView', isPublic: true},
     '/a': {view: a, name: 'A', isPublic: true},    
-    '/b': {view: b, name: 'B', isPublic: true},
-<<<<<<< HEAD
-    '/blog': {view: blog, name: 'blog', isPublic: true},   
-    '/lista': {view: blog, name: 'lista', isPublic: true},   
-=======
     '/blog': {view: blog, name: 'blog', isPublic: true},
     '/test': {view: TestView, name: 'lista', isPublic: true},
     '/registro': {view: Register, name: 'registro', isPublic: true},
-
->>>>>>> 90d3de76ab9c9bb87f582c5c331998459ebb2d6d
 }
